9d09d19d3e9b2b9075a4a392d9fcb593c17b96c4
Georg Graf (SPG) (acer) <grafg@spengergasse.at>
2025-10-30 14:04:28 +0100

    akhrab

--- convert.sh
+++ convert.sh
@@ -1,4 +1,4 @@
 pandoc Mermaid_Graph.md -f gfm -t html5 -s \
-  --include-in-header mermaid-header.html \
-  --lua-filter mermaid-codeblock.lua \
+  --include-in-header ~/svn/georg/EDV/spg-gfm/mermaid-header.html \
+  --lua-filter ~/svn/georg/EDV/spg-gfm/mermaid-codeblock.lua \
   -o out.html
